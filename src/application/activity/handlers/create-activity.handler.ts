4c1d726d76c35f2041172f5606e9b5cfa2e81057
FelipeCastro5 <danielfelipecastrolizarazo@gmail.com>
2025-07-06 23:49:13 -0500

    [DC] second commit

---
+++ src/application/activity/handlers/create-activity.handler.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateActivityCommand } from '../commands/create-activity.command';
+import { ActivityInterface } from '../../../domain/activity-domain/activity.interface';
+import { ResponseUtil } from '../../utilities/response.util';
+
+@Injectable()
+export class CreateActivityHandler {
+  constructor(
+    @Inject('ActivityInterface')
+    private readonly activityRepository: ActivityInterface,
+  ) { }
+
+  async execute(command: CreateActivityCommand) {
+    try {
+      const newActivity = await this.activityRepository.createActivity(
+        command.fk_user,
+        command.fk_proj,
+        command.activity,
+      );
+      return ResponseUtil.success(newActivity, 'Actividad creada exitosamente', 201);
+    } catch (error) {
+      console.error('Error en CreateActivityHandler:', error);
+      return ResponseUtil.error('Error al crear la actividad', 500);
+    }
+  }
+}
