3ab9a8fee77ce50b523a1184a71f8e4b00399c26
FelipeCastro5 <danielfelipecastrolizarazo@gmail.com>
2025-07-08 00:26:17 -0500

    [DC] DeleteEntities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './presentation/modules/user.module';
 import { GoogleDriveModule } from './infrastructure/google-drive-api/google-drive.module';
-import { ProjectsModule } from './presentation/modules/projects.module';
 import { GeminiModule } from './infrastructure/gemini-ia/gemini.module';
 import { ConfigModule } from '@nestjs/config';
 import { HistorialModule } from './presentation/modules/historial.module';
-import { ActivitiesModule } from './presentation/modules/activities.module';
 import { iaModule } from './infrastructure/ia-service/ia.module';
 
 @Module({
   imports: [
     iaModule,
-    UsersModule,
-    ProjectsModule,
-    ActivitiesModule,
     HistorialModule,
     GoogleDriveModule,
     ConfigModule.forRoot({ isGlobal: true }), // 👈 carga automáticamente .env
