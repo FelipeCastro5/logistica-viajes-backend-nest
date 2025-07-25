bed71af90866259aaf5b9fe3d2cbabe568c2db72
FelipeCastro5 <danielfelipecastrolizarazo@gmail.com>
2025-07-25 13:01:51 -0500

    [DC] Readme

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://logistica-viajes-frontend-react-vit.vercel.app'],  //'*',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
